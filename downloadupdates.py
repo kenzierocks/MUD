1858b7250fc764b979e5bcd61335d99ba9a02c61
Kenzie Togami <ket1999@gmail.com>
2015-03-31 10:01:54 -0700

    Make the gradle call a little better

--- downloadupdates.py
+++ downloadupdates.py
@@ -2,8 +2,12 @@ __author__ = 'Kenzie Togami'
 import data.mods as modcfg
 import fetch
 import subprocess
+import os
 
 for mod in modcfg.MODS:
     fetch.fetch_site_and_process(mod)
 
-subprocess.check_call("gradle", shell=True)
+print('Launching gradle...')
+env = dict(os.environ)
+env['GRADLE_INCREMENTAL'] = 'false'
+subprocess.check_call("gradle --daemon -i -x buildLauncher", env=env, shell=True)
