ae1862be11487334c0277a4ede33d494521df4c7
Kenzie Togami <ket1999@gmail.com>
2015-03-31 10:02:04 -0700

    Sort mods by alphabetical

--- data/mods.py
+++ data/mods.py
@@ -2,9 +2,9 @@ __author__ = 'Kenzie Togami'
 from modlib import Mod
 
 
-mod_buildcraft = Mod(name='Buildcraft', site='http://www.mod-buildcraft.com/releases/BuildCraft/')
-mod_ic2 = Mod(name='IndustrialCraft2',
-              site='http://jenkins.ic2.player.to/job/IC2_experimental/lastSuccessfulBuild/artifact/build/libs/')
+MODS = sorted([Mod(name='Buildcraft', site='http://www.mod-buildcraft.com/releases/BuildCraft/'),
+               Mod(name='IndustrialCraft2',
+                   site='http://jenkins.ic2.player.to/job/IC2_experimental/lastSuccessfulBuild/artifact/build/libs/'),
+              ], key=lambda mod: mod.name)
 
-
-MODS = [y for x, y in dict(locals()).items() if x.startswith('mod_')]
+__all__ = ['MODS']
